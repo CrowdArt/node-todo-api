fe8dce1767d8f07654f8d9651a1a2ff0584414ca
[Vuk Djukic] <[vukdukic@gmail.com]>
2018-09-28 08:41:05 -0700

    todo.js and user.js

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,46 +1,35 @@
 // const MongoClient = require('mongodb').MongoClient;
-const {MongoClient, ObjectID}= require('mongodb');
+const {MongoClient, ObjectID} = require('mongodb');
 
-/** 
-var obj = new ObjectID();
-console.log(obj);
-*/
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
 
-/** 
-// destrucuring objects - E6 feature
-var user = {name: 'andrew', age: 25};
-var {name} = user;
-console.log(name);
-*/
-// connect to database
-MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
-    if (err) {
-      return console.log('Unable to connect to MongoDB server');
-    }
-    console.log('Connected to MongoDB server');
-/** 
-    db.collection('Todos').insertOne({
-        text: 'Something to do',
-        completed: false
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert todo', err);
-        }
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
 
-        console.log(JSON.stringify(result.ops, undefined,2 ));
-    });
-// Insert new doc into Users (name, age, location)
-    db.collection('Users').insertOne({
-        name: 'Vuk',
-        age: 41,
-        location: 'San Francisco'
-    }, (err, result) => {
-        if (err) {
-            return console.log('Unable to insert user', err);
-        }
+  // Insert new doc into Users (name, age, location)
+  // db.collection('Users').insertOne({
+  //   name: 'Andrew',
+  //   age: 25,
+  //   location: 'Philadelphia'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert user', err);
+  //   }
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
-        console.log(result.ops);
-    });
-*/
-    db.close();
+  db.close();
 });
--- server/server.js
+++ server/server.js
@@ -1,5 +1,26 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/ToDoApp');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
